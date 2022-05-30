8b774ba1e294fc90b5b56ba7b667ee6de7498817
bai <baiyshdykanov6@gmail.com>
2022-05-30 18:18:34 +0300

    added burger-menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Header.css"
 import logo from "../../assets/images/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
 import {Link} from "react-router-dom";
 const Header = () => {
+    const [active,setActive] = useState(false)
     return (
         <div>
         <header>
             <div className="container">
                 <div className="header__container">
+                    <div className={active? 'burger show-menu':'burger' } onClick={() => setActive(!active)}>
+                    <span />
+                </div>
+
                   <Link to="/"><img src={logo} alt="movieImg" width="160" height="20" /></Link>
-                    <nav className="menu">
+                    <nav className={active? 'menu show' : 'menu' } onClick={() => setActive(false)}>
                         <Link to="/films" >Фильмы</Link>
                         <Link to="/serials" >Сериалы</Link>
                     </nav>
--- src/pages/Films/Films.jsx
+++ src/pages/Films/Films.jsx
@@ -3,14 +3,19 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
 import PopularMovie from "../../components/PopulerMovie";
-
+import Spinner from "../../components/Spinner";
+import bg from  "../../assets/images/bg.c5b8afb48b289b6523dd.svg"
 const Films = () => {
     const [time,setTime] = useState("day")
+    const [isLoading,setIsLoading] = useState(true)
     const [trends,setTrends] = useState([])
     const [active,setActive] = useState(false)
     useEffect(() => {
         axios(`https://api.themoviedb.org/3/trending/movie/${time}?language=ru&api_key=042f11beb984d2ca7828fd2109953f49`)
-            .then(({data}) => setTrends(data.results))
+            .then(({data}) => {
+                setTrends(data.results)
+                setIsLoading(false)
+            })
     },[time])
     const formatDate = (date) => {
         const month = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
@@ -18,36 +23,43 @@ const Films = () => {
         reversedDate[1] = month[reversedDate[1] - 1]
         return reversedDate.join(' ')
     }
+    if(isLoading) {
+        return <Spinner />
+    }
     return (
-    <div className="container">
+    <div className="container" >
     <PopularMovie />
        <div className="films-buttons">
            <h2 className="films-title">Тренды</h2>
-           <button onClick={() => setActive(!active) || setTime("day")}  className={active?"selector active":"selector"} type="button">Сегодня</button>
+           <button  onClick={() => setActive(!active) || setTime("day")}  className={active?"selector active":"selector"} type="button">Сегодня</button>
            <button onClick={() => setActive(!active) || setTime("week")} className={!active?"selector active":"selector"} type="button">На этой неделе</button>
        </div>
-        <div className="scroller">
-            {
-                trends.map((item) => (
-                    <div  key={item.id} className="movie-card">
-                        <div className="card-img">
-                            <Link  to={`/movie/${item.id}`}>
-                                <img src={`https://image.tmdb.org/t/p/w440_and_h660_face${item.poster_path}`} alt=""/>
-                            </Link>
-                            <div className="consensus">
-                                <div className="info-rating">{item.vote_average}</div>
+            <div className="scroller" style={{
+                backgroundImage:`url(${bg})`,
+                backgroundRepeat:"no-repeat",
+                backgroundPosition:"50% 150px"
+            }}>
+                {
+                    trends.map((item) => (
+                        <div  key={item.id} className="movie-card">
+                            <div className="card-img">
+                                <Link  to={`/movie/${item.id}`}>
+                                    <img src={`https://image.tmdb.org/t/p/w440_and_h660_face${item.poster_path}`} alt=""/>
+                                </Link>
+                                <div className="consensus">
+                                    <div className="info-rating">{item.vote_average}</div>
+                                </div>
+                            </div>
+                            <div className="card-content">
+                                <Link to={`/movie/${item.id}`}>
+                                    <h5 className="card-title">{item.title}</h5>
+                                </Link>
+                                <span className="card-year">{formatDate(item.release_date)}</span>
                             </div>
                         </div>
-                        <div className="card-content">
-                            <Link to={`/movie/${item.id}`}>
-                                <h5 className="card-title">{item.title}</h5>
-                            </Link>
-                            <span className="card-year">{formatDate(item.release_date)}</span>
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
+                    ))
+                }
+            </div>
     </div>
     );
 };
